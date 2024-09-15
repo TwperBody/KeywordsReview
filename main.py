723794177a09d4807c7fe7c1ddd3ae38a5f6fb8f
TwperBody <mcjiekejiemi@163.com>
2024-09-15 20:19:36 +0800

    主要程序

--- main.py
+++ main.py
@@ -1,48 +1,59 @@
+from pkg.plugin.models import *
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *  # 导入事件类
 
+import json
+
+message_string = ""
+
+file_path = 'plugins\keywordsReview\keywords.json'
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
+@register(name="Keyword_active_review", description="关键词主动审查", version="1.0", author="TwperBody")
+class KeywordActiveReviewPlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
         pass
 
-    # 异步初始化
     async def initialize(self):
         pass
 
-    # 当收到个人消息时触发
+    # 当收到user消息时触发
     @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
+    async def PersonNormalMessageReceived(self, ctx: EventContext):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        keywords = data.get('keywords', [])
         msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+        for keyword_dict in keywords:
+            message_string = keyword_dict.get('keyword', '')
+            if message_string in msg:
+                self.ap.logger.debug("{} is trigger keyword".format(ctx.event.sender_id))
+                ctx.add_return("reply", ["客户端拒绝请求, {}!".format(ctx.event.sender_id)])
+                # 阻止该事件默认行为（向接口获取回复）
+                ctx.prevent_default()
+                break
 
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
 
     # 当收到群消息时触发
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+        keywords = data.get('keywords', [])
+        if " " or "@" in msg:
+            msg = ctx.event.text_message  # 这里的 event 即为 GroupMessageReceived 的对象
+            for keyword_dict in keywords:
+                message_string = keyword_dict.get('keyword', '')
+                if message_string in msg:
+                    self.ap.logger.debug("{} is trigger keyword".format(ctx.event.sender_id))
+                    ctx.add_return("reply", ["客户端拒绝请求, {}!".format(ctx.event.sender_id)])
+                    # 阻止该事件默认行为（向接口获取回复）
+                    ctx.prevent_default()
+                    break
 
     # 插件卸载时触发
     def __del__(self):
